89ae6b4126058f436e023288529aab5a80b343aa
shanhong cheng <shnahongcheng@gmail.com>
2017-03-31 19:02:41 -0600

    stretch mode

--- app/src/main/java/education/loganfreeman/com/bestreading/GenreAdapter.java
+++ app/src/main/java/education/loganfreeman/com/bestreading/GenreAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -60,10 +61,10 @@ class GenreAdapter extends BaseAdapter {
 
 
     private class MyViewHolder {
-        Button tvTitle;
+        TextView tvTitle;
 
         public MyViewHolder(View item) {
-            tvTitle = (Button) item.findViewById(R.id.tvTitle);
+            tvTitle = (TextView) item.findViewById(R.id.tvTitle);
 
         }
     }
--- app/src/main/java/education/loganfreeman/com/bestreading/MainActivity.java
+++ app/src/main/java/education/loganfreeman/com/bestreading/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.GridView;
 
 import org.mozilla.javascript.tools.debugger.Main;
