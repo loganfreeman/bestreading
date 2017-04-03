f40e5c347b3a7369660d694bc84bfeb0d5b25b02
shanhong cheng <shanhong.cheng@utah.edu>
2017-04-03 14:16:14 -0600

    all

--- app/src/main/java/education/loganfreeman/com/bestreading/NovelDetailActivity.java
+++ app/src/main/java/education/loganfreeman/com/bestreading/NovelDetailActivity.java
@@ -3,17 +3,24 @@ package education.loganfreeman.com.bestreading;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.parceler.Parcels;
 
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import education.loganfreeman.com.bestreading.base.BaseActivity;
+import education.loganfreeman.com.bestreading.speech.Speaker;
 import education.loganfreeman.com.bestreading.utils.PLog;
+import education.loganfreeman.com.bestreading.utils.ToastUtil;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
@@ -40,6 +47,19 @@ public class NovelDetailActivity extends BaseActivity {
     @BindView(R.id.novel_textview)
     TextView text;
 
+    @BindView(R.id.action_play)
+    FloatingActionButton playBtn;
+
+    @BindView(R.id.action_pause)
+    FloatingActionButton pauseBtn;
+
+    private final int CHECK_CODE = 0x1;
+    private final int LONG_DURATION = 5000;
+    private final int SHORT_DURATION = 1200;
+
+    private Speaker speaker;
+
+
     public static void start(Context context, Novels.Genre genre, String url) {
         Intent intent = new Intent(context, NovelDetailActivity.class);
         intent.putExtra(NOVEL_GENRE, Parcels.wrap(genre));
@@ -56,11 +76,72 @@ public class NovelDetailActivity extends BaseActivity {
         url = getIntent().getStringExtra(NOVEL_URL);
         PLog.i("Url " + url);
         initView();
+        checkTTS();
 
+
+
+    }
+    @Override
+    public void onPause(){
+        if(speaker != null) {
+            speaker.stop();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(speaker != null) {
+            speaker.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void checkTTS(){
+        Intent check = new Intent();
+        check.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(check, CHECK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CHECK_CODE){
+            if(resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS){
+                speaker = new Speaker(this);
+                playBtn.setEnabled(true);
+                pauseBtn.setEnabled(true);
+            }else {
+                Intent install = new Intent();
+                install.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(install);
+            }
+        }
     }
 
     private void initView() {
         safeSetTitle(genre.getTitle());
+        playBtn.setEnabled(false);
+        pauseBtn.setEnabled(false);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = text.getText().toString();
+                //Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                String lines[] = toSpeak.split("\\r?\\n");
+                for(String line : lines) {
+                    speaker.speak(line);
+                }
+
+            }
+        });
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speaker.pause(LONG_DURATION);
+            }
+
+
+        });
         Novels.getArticleAsync(url)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
---
+++ app/src/main/java/education/loganfreeman/com/bestreading/speech/Speaker.java
@@ -0,0 +1,94 @@
+package education.loganfreeman.com.bestreading.speech;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+import education.loganfreeman.com.bestreading.MainActivity;
+import education.loganfreeman.com.bestreading.utils.ToastUtil;
+
+/**
+ * Created by shanhong on 4/3/17.
+ */
+
+public class Speaker implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+
+    private boolean ready = false;
+
+    private boolean allowed = true;
+
+    public Speaker(Context context){
+        tts = new TextToSpeech(context, this);
+    }
+
+    public boolean isAllowed(){
+        return allowed;
+    }
+
+    public void allow(boolean allowed){
+        this.allowed = allowed;
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS){
+            // Change this to match your
+            // locale
+            tts.setLanguage(Locale.US);
+            ready = true;
+            ToastUtil.showShort("Speaker ready!");
+
+        }else{
+            ready = false;
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+
+
+    public void speak(String text){
+
+        // Speak only if the TTS is ready
+        // and the user has allowed speech
+
+        if(ready && allowed) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Bundle params = new Bundle();
+
+                tts.speak(text, TextToSpeech.QUEUE_ADD, params, "UniqueID");
+            }else{
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "UniqueID");
+                tts.speak(text, TextToSpeech.QUEUE_ADD, map);
+            }
+        }
+    }
+
+    public void pause(int duration){
+        tts.playSilence(duration, TextToSpeech.QUEUE_ADD, null);
+    }
+
+    // Free up resources
+    public void destroy(){
+        tts.shutdown();
+    }
+
+    public void stop() {
+        tts.stop();
+    }
+
+
+}
